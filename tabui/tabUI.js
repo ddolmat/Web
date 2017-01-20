27d08b3fba122959c1626ae56f1d1125a5f41002
Chunho Park <faterider@naver.com>
2017-01-20 19:11:13 +0900

    Add files via upload

---
+++ tabui/tabUI.js
@@ -0,0 +1,53 @@
+
+
+function ajax(url, id){
+	var oReq = new XMLHttpRequest();
+	var secId = id;
+	oReq.addEventListener("load", inputContent);
+	oReq.open("GET", url);
+	oReq.send();
+}
+function inputContent(){
+	var temp = JSON.parse(this.responseText);
+	var content = "<dt>"+temp["title"]+"</dt><dd>"+temp["body"]+"</dd>";
+	var target = document.querySelector(".eleDisplayShow");
+	target.innerHTML = "";
+	target.insertAdjacentHTML("beforeend", content);
+}
+
+function classSwap(oldNode, newNode, dClass) {
+	oldNode.classList.remove(dClass);
+	newNode.classList.add(dClass);
+}
+
+function changeView(evt) {
+	var evtTarget = evt.target;
+	if(evtTarget.tagName !== "SPAN" && evtTarget.tagName !== "DIV") return;
+	if(evtTarget.tagName === "SPAN" ) evtTarget = evtTarget.parentElement;
+	var sTab = nav.querySelector(".selectedTab");
+	classSwap(sTab, evtTarget, "selectedTab");
+	var secId = "my_"+evtTarget.id;
+	var dpSec = document.querySelector(".eleDisplayShow");
+	var newSec = document.querySelector("#"+secId);
+	classSwap(dpSec, newSec, "eleDisplayShow");
+	ajax(urlList[secId], secId);
+}
+
+
+function displaySection(id) {
+	var dpSec = document.querySelector(".eleDisplayShow");
+	dpSec.classList.remove("eleDisplayShow");
+	document.querySelector("#"+id).classList.add("eleDisplayShow");
+}
+var urlList = {
+	"my_position" : "http://jsonplaceholder.typicode.com/posts/1",
+	"my_friend" : "http://jsonplaceholder.typicode.com/posts/2",
+	"my_theme" : "http://jsonplaceholder.typicode.com/posts/3",
+	"my_news" : "http://jsonplaceholder.typicode.com/posts/4"
+}
+
+var nav = document.querySelector(".mainView > nav");
+
+nav.addEventListener("click", changeView);
+
+
